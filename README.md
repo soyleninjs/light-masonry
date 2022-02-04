b7b06c1022ff89f6d2ca8f4de36221d006dc801f
soyleninjs <leninfelix@hotmail.com>
2022-02-04 09:19:30 -0600

    v1.0.0

---
+++ README.md
@@ -0,0 +1,121 @@
+# light-masonry
+
+[![npm](https://img.shields.io/npm/v/light-masonry?color=check&style=plastic)](https://www.npmjs.com/package/light-masonry)
+[![npm bundle size](https://img.shields.io/bundlephobia/min/light-masonry?style=plastic)](https://www.npmjs.com/package/light-masonry)
+
+Script para crear un layout tipo masonry.
+
+Solo es necesario tener el contenedor junto a sus hijos que se acomodaran en este tipo de layout.
+
+El script formateara todo, agregara y colocara lo necesario para que funcione el layout.
+
+## Demo
+
+**[Codepen](https://codepen.io/soyleninjs/pen/GRMdQqp)**
+
+## CSS
+
+- Este script necesita algunos styles base:
+
+  ```css
+  /* ------------------ light-masonry.css ------------------ */
+  .light-masonry-wrapper {
+    --gap: 5px;
+    --gap-between-columns: var(--gap);
+    --gap-between-items: var(--gap);
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
+    width: 100%;
+    grid-gap: var(--gap-between-columns);
+  }
+
+  .light-masonry-column {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+  }
+
+  .light-masonry-column .light-masonry-item {
+    width: 100%;
+  }
+
+  .light-masonry-column .light-masonry-item:not(:last-child) {
+    margin-bottom: var(--gap-between-items);
+  }
+  ```
+
+  **Aquí hay 3 variables css que podemos manipular**:
+
+  - **—gap**
+    El valor de este cambia los espacios tanto entre las filas y las columnas, totalmente parejo.
+  - **—gap-between-columns**
+    El valor de este cambia solo los espacios entre cada columna. _Eje horizontal_
+  - **—gap-between-items**
+    El valor de este cambia solo los espacios entre cada item. _Eje vertical_
+    **NOTA:** Estas variables van en la misma clase que le pasaste al script.
+
+## Parámentros
+
+- **containerClass [Required] [String]**
+  Este parametro recibe la clase del contenedor al que se configurara el script.
+  ```javascript
+  const container = ".main-container-masonry";
+  ```
+- **Options [Optional] [Object]**
+  Él parámetro que recibe es un objeto con todas las opciones posibles. Cada que agregues una option esta se actualizara la default.
+  ```javascript
+  const options = {
+    defaultColumns: 4,
+    responsive: {
+      1440: 4,
+      834: 3,
+      680: 2,
+    },
+    init: (data) => {},
+    afterBreakpoint: (data) => {},
+  };
+  ```
+  - **defaultColumns [Optional] [Number]**
+    Este campo sirve para colocar las columnas por default que tendrá el layout dado caso no se pasen medidas responsive o si las medidas responsive dadas ya no se se cumplen
+    ```javascript
+    const options = {
+      defaultColumns: 5,
+    };
+    ```
+  - **responsive [Optional] [Object]**
+    Este parámetro sirve para agregar las medidas responsive y las columnas que abra en cada query.
+    Se basa en **desktopFirst,** es decir, una vez que alcanza la medida este se configura hacia abajo, dando el numero de columnas pasado a esta medida, y cambia una vez alcance la siguiente medida.
+    ```javascript
+    const options = {
+      responsive: {
+        // window.width <= 1440px : 4 columns
+        1440: 4,
+        // window.width <= 834px : 3 columns
+        834: 3,
+        // window.width <= 680px : 2 columns
+        680: 2,
+      },
+    };
+    ```
+  - **init [Optional] [function]**
+    Retorna el objeto con la información actualmente en ejecución.
+    Se lanza justo después de configurar el script por primera vez.
+    ```javascript
+    const options = {
+      init: (data) => {
+        console.log(data);
+      },
+    };
+    ```
+  - **afterBreakpoint [Optional] [function]**
+    Retorna el objeto con la información actualmente en ejecución.
+    Se lanza justo después de cada cambio de breakpoint.
+    ```javascript
+    const options = {
+      afterBreakpoint: (data) => {
+        console.log(data);
+      },
+    };
+    ```
---
+++ js/light-masonry.js
@@ -0,0 +1,213 @@
+/* eslint-disable no-empty-function, no-unused-vars */
+/* light-masonry.js v1.0.0 */
+
+/*
+
+  agregar configuracion para Mode:
+  normal (default) - orden normal, de izquierda a derecha
+  reverse - orden invertido, de derecha a izquierda
+  vertical-fill - orden de izquierda a derecha pero llenando hacia las columnas con menos altura
+  
+  Configuracion para diferentes anchos de hijos.
+
+*/
+
+const lightMasonry = (containerClass, newOptions = {}) => {
+  // ------------------------ VARAIBLES ------------------------
+
+  const $masonryWrapper = document.querySelector(containerClass);
+  const hiddenBox =
+    '<div class="light-masonry-hidden-box" style="display: none !important;"></div>';
+  const column = '<div class="light-masonry-column"></div>';
+  const classItemColumn = "light-masonry-item";
+  const classWrapper = "light-masonry-wrapper";
+  const classInitialized = "light-masonry-initialized";
+  const dataCallback = {};
+  let firstExecution = false;
+  let allItems = [];
+  const defaultOptions = {
+    defaultColumns: 4,
+    responsive: {
+      1440: 4,
+      834: 3,
+      680: 2,
+    },
+    init: (data) => {},
+    afterBreakpoint: (data) => {},
+  };
+  const finalOptions = { ...defaultOptions, ...newOptions };
+
+  // ------------------------ END VARAIBLES ------------------------
+
+  // ------------------------ FUNCTIONALITY ------------------------
+
+  const setOrder = () => {
+    const $masonryItems = $masonryWrapper.querySelectorAll(
+      ".light-masonry-item"
+    );
+
+    $masonryItems.forEach(($element, index) => {
+      $element.dataset.index = index;
+    });
+  };
+
+  const setChildsClass = () => {
+    Array.from($masonryWrapper.children).forEach(($element) => {
+      $element.classList.add(classItemColumn);
+    });
+  };
+
+  const setWrapperClass = () => {
+    $masonryWrapper.classList.add(classWrapper);
+  };
+
+  const setInitializedClass = () => {
+    $masonryWrapper.classList.add(classInitialized);
+  };
+
+  const setAttributes = () => {
+    $masonryWrapper.setAttribute("data-breakpoint", dataCallback.breakpoint);
+    $masonryWrapper.setAttribute("data-columns", dataCallback.columns);
+  };
+
+  const createHiddenBox = () => {
+    $masonryWrapper.insertAdjacentHTML("beforeend", hiddenBox);
+  };
+
+  const getItems = () => {
+    const $masonryItems = $masonryWrapper.querySelectorAll(
+      ".light-masonry-item"
+    );
+
+    $masonryItems.forEach(($element) => {
+      $masonryWrapper
+        .querySelector(".light-masonry-hidden-box")
+        .appendChild($element);
+    });
+  };
+
+  const removeColumns = () => {
+    $masonryWrapper
+      .querySelectorAll(".light-masonry-column")
+      .forEach(($element) => {
+        $element.remove();
+      });
+  };
+
+  const addColumns = (columns) => {
+    for (let i = 1; i <= columns; i++) {
+      $masonryWrapper.insertAdjacentHTML("beforeend", column);
+    }
+  };
+
+  const setItems = () => {
+    const $arrayIndex = [];
+    const $masonryItems = $masonryWrapper.querySelectorAll(
+      ".light-masonry-item"
+    );
+
+    $masonryItems.forEach(($element) => {
+      $arrayIndex.push($element.dataset.index);
+    });
+
+    allItems = $arrayIndex.sort((a, b) => a - b);
+
+    const setItemsInColum = ($column) => {
+      if (!allItems.length) {
+        return;
+      }
+
+      $column.appendChild(
+        $masonryWrapper.querySelector(
+          `.light-masonry-item[data-index="${allItems[0]}"]`
+        )
+      );
+      allItems.shift();
+    };
+
+    while (allItems.length) {
+      const $columns = $masonryWrapper.querySelectorAll(
+        ".light-masonry-column"
+      );
+      $columns.forEach(setItemsInColum);
+    }
+  };
+
+  const setInitCallback = (data) => {
+    if (firstExecution) {
+      return;
+    }
+    finalOptions.init(data);
+    firstExecution = true;
+  };
+
+  const setBreakpointCallback = (data) => {
+    if (firstExecution) {
+      finalOptions.afterBreakpoint(data);
+    }
+  };
+
+  const setLayout = () => {
+    let finalColumns = finalOptions.defaultColumns;
+    if (finalOptions.responsive !== undefined) {
+      Object.keys(finalOptions.responsive)
+        .reverse()
+        .forEach((query) => {
+          if (window.innerWidth > Number(query)) {
+            return;
+          }
+
+          dataCallback.breakpoint = Number(query);
+          finalColumns = finalOptions.responsive[query];
+        });
+    }
+
+    if (dataCallback.columns === finalColumns) {
+      dataCallback.columns = finalColumns;
+      if (!firstExecution) {
+        getItems();
+        removeColumns();
+        addColumns(finalColumns);
+        setItems();
+        setAttributes();
+      }
+    } else {
+      dataCallback.columns = finalColumns;
+      getItems();
+      removeColumns();
+      addColumns(finalColumns);
+      setItems();
+      setAttributes();
+      setBreakpointCallback(dataCallback);
+    }
+  };
+
+  // ------------------------ END FUNCTIONALITY ------------------------
+
+  // ------------------------ BASE CONFIG ------------------------
+
+  setWrapperClass();
+  setChildsClass();
+  setOrder();
+  createHiddenBox();
+
+  // ------------------------ END BASE CONFIG ------------------------
+
+  // ------------------------ INIT ------------------------
+
+  setLayout();
+  setInitializedClass();
+  setInitCallback(dataCallback);
+
+  // ------------------------ END INIT ------------------------
+
+  // ------------------------ EVENTS ------------------------
+
+  window.addEventListener("resize", setLayout);
+
+  // ------------------------ END EVENTS ------------------------
+};
+
+window.lightMasonry = lightMasonry;
+
+export default lightMasonry;
